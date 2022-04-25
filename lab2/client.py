5b6f8e40366d0a36a26229bdd8e6ad63bbd9b270
Luan Martins <luan.martins@parfin.io>
2022-04-25 19:36:51 -0300

    lab2

--- lab2/client.py
+++ lab2/client.py
@@ -7,9 +7,16 @@ s = socket.socket()
 
 s.connect((HOST, PORT))
 
+# The client should receive a message, containing the list of all the files
+print(str(s.recv(2048), encoding='utf-8'))
 msg = input('')
+
+# And send a message, with the chosen option
 s.send(bytearray(msg, 'utf-8'))
+
+# Then receive the correspondent list of the frequent words
 l = s.recv(2048)
 print(str(l, encoding='utf-8'))
 
+# And finally close the connection
 s.close()
--- lab2/server.py
+++ lab2/server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, glob
 
 HOST = ''
 PORT = 7776
@@ -13,42 +13,67 @@ s.listen(1)
 print('Servidor inicializado')
 
 while True:
+  print('Aguardando nova conexão...')
+
   ns, address = s.accept()
 
   print(f'Servidor conectado com {address}', )
 
-  msg = ns.recv(2048)
-  print(str(msg, encoding='utf-8'))
-
-  try:
-    with open(msg, 'r') as f:
-
-      frequencies = {}
-
-      # Add every word from the file into a dictionary to count frequencies
-      for line in f:
-        for word in line.split():
-          lowerWord = word.lower()
-          if frequencies.__contains__(lowerWord): frequencies[lowerWord] += 1 
-          else: frequencies[lowerWord] = 1
-      
-      # Sort frequencies dictionary based on value, decreasing order
-      frequencies = dict(sorted(frequencies.items(), key=lambda p: p[1], reverse=True))
-
-      # Prepare the message containing 5 words, from most frequent to least frequent
-      l = ''
-      i = 0
-      for word in frequencies.keys():
-        l += word
-        i += 1
-        if i == 5: break
-        l += '\n'
-
-      ns.send(bytearray(l, encoding='utf-8'))
+  # Send message listing all options
+  files = {}
+  fileCount = 0
+  for file in glob.glob("./*.txt"):
+    files[fileCount] = file
+    fileCount += 1
   
-  except IOError: 
-    # In case of an error with the file
-    ns.send(b'Nome de arquivo inexistente')
+  options = 'Escolha sua opção (número):'
+  for key in files:
+    options += f'\n{str(key)}: {files[key]}'
+
+  ns.send(bytearray(options, encoding='utf-8'))
+
+
+  msg = ns.recv(2048)
+  msg = str(msg, encoding='utf-8')
+  print(f'Opção escolhida: {msg}.')
+
+  # In case of an error with the file
+  try:
+    if msg.isnumeric() == False:
+      raise Exception()
+    elif files.__contains__(int(msg)) == False:
+      raise Exception()
+  except:
+    print('Opção inválida')
+    ns.send(bytearray('Opção inválida', encoding='utf-8')) 
+    ns.close()
+    continue
+
+
+  f = open(files[int(msg)], 'r')
+
+  frequencies = {}
+
+  # Add every word from the file into a dictionary to count frequencies
+  for line in f:
+    for word in line.split():
+      lowerWord = word.lower()
+      if frequencies.__contains__(lowerWord): frequencies[lowerWord] += 1 
+      else: frequencies[lowerWord] = 1
+  
+  # Sort frequencies dictionary based on value, decreasing order
+  frequencies = dict(sorted(frequencies.items(), key=lambda p: p[1], reverse=True))
+
+  # Prepare the message containing 5 words, from most frequent to least frequent
+  l = ''
+  i = 0
+  for word in frequencies.keys():
+    l += word
+    i += 1
+    if i == 5: break
+    l += '\n'
+
+  ns.send(bytearray(l, encoding='utf-8'))
 
   ns.close()
 s.close()
